416d9bb9ab95270771099676ff1076adbbca0b57
Tom Hensel <github@jitter.eu>
2018-01-01 02:45:59 +0100

    colors are nice
    [skip ci]

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ p_led.value(1)
 p_pir = Pin('G9', mode=Pin.IN, pull=Pin.PULL_UP)
 p_slp = Pin('G8', mode=Pin.OUT)
 # disable sleep
+p_slp.value(0)
+time.sleep_ms(10)
 p_slp.value(1)
 # buzzer
 p_buz = Pin('G5', mode=Pin.OUT)
@@ -32,36 +34,42 @@ def settimeout(duration):
     print('mqtt: timeout')
     pass
 
+MQTT_ID="sensor-hall"
+MQTT_HOST="mqtt.jitter.local"
+
 print('mqtt: connect')
 # TODO: abstraction
-client = MQTTClient("sensor-01", "mqtt.jitter.local", port=1883)
+client = MQTTClient(MQTT_ID, MQTT_HOST, port=1883)
 client.settimeout = settimeout
 client.connect()
 
-client.publish("home/sensor/hall/status", "online")
-client.publish("home/sensor/hall/vbatt", str(get_vbatt()))
+def alive():
+    client.publish("home/sensor/hall/vbatt", str(get_vbatt()))
+    client.publish("home/sensor/hall/status", "True")
+
+alive()
 
 def p_btn_handler(arg):
-    #print(arg)
     if p_btn.value() == 1:
         btn_state = False
+        time.sleep_ms(50)
     else:
         btn_state = True
-        pycom.rgbled(0x002020)
-        time.sleep_ms(50)
+        pycom.rgbled(0x001010)
+        time.sleep_ms(10)
         pycom.rgbled(0x0)
     client.publish("home/sensor/hall/button", str(btn_state))
 
 def p_pir_handler(arg):
-    #print(arg)
+    print(arg)
     if p_pir.value() == 1:
         pir_state = False
-        pycom.rgbled(0x002000)
+        pycom.rgbled(0x004000)
         time.sleep_ms(50)
         pycom.rgbled(0x0)
     else:
         pir_state = True
-        pycom.rgbled(0x202000)
+        pycom.rgbled(0x400000)
         time.sleep_ms(50)
         pycom.rgbled(0x0)
     client.publish("home/sensor/hall/motion", str(pir_state))
@@ -71,9 +79,9 @@ p_btn.callback(Pin.IRQ_FALLING | Pin.IRQ_RISING, p_btn_handler)
 
 i = 0
 while True:
-    i =+ 1
-    if(i >= 600):
-        client.publish("home/sensor/hall/vbatt", str(get_vbatt()))
+    if i >= 21600:
+        alive()
         i = 0
     machine.idle()
-    time.sleep(0.5)
+    time.sleep_ms(100)
+    i =+ 1
